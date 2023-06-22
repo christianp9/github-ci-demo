b4ff961ff57eb5b8b8d60462eb4566fdb5a9bde6
christianPearson <cpearson@mutualser.org>
2023-06-21 21:44:47 -0500

    corrigiendo test

--- __test__/helloworld.test.js
+++ __test__/helloworld.test.js
@@ -1,10 +1,10 @@
-/*const request = require('supertest');
+const request = require('supertest');
 const app = require('../app');
 describe('GET /', () => {
     it('responde con el resultado de a + b es =5', done => {
         request(app)
             .get('/suma')
             .expect('la suma de a + b es = 5', done);
+        done()
     });
 });
-*/
