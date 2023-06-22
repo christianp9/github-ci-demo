71b31892f883c70d2228cddf199eb42f209b21f7
christianPearson <cpearson@mutualser.org>
2023-06-21 19:19:51 -0500

    arreglando test

--- __test__/helloworld.test.js
+++ __test__/helloworld.test.js
@@ -3,7 +3,7 @@ const app = require('../core/infrastructure/controller/suma');
 describe('GET /', () => {
     it('responde con el resultado de a + b es =5', done => {
         request(app)
-            .get('/')
+            .get('/suma')
             .expect('el resultado de a + b es =5', done);
     });
 });
