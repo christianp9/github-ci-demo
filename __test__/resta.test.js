f01628a11ef003eac03634da880f7de7bef847fe
christianPearson <cpearson@mutualser.org>
2023-06-25 19:39:32 -0500

    corrigiendo test

--- __test__/resta.test.js
+++ __test__/resta.test.js
@@ -2,10 +2,12 @@ const request = require('supertest');
 const operacionesService = require('../operation-service');
 describe('GET /', () => {
 
-    it('responde con el resultado de a + b es = 5', done => {
+    it('responde con el resultado de la resta de a - b es = 1', done => {
         request(operacionesService)
-            .get('/suma')
-            .expect('la suma de a + b es = 5', done);
+            .get('/resta')
+            .expect('la resta de a - b es = 1', done);
         done()
     });
 });
+
+
