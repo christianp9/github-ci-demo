5ef8653a8c95ab86084e737a70d8559b1d997190
christianPearson <cpearson@mutualser.org>
2023-06-25 19:30:03 -0500

    corrigiendo test

--- __test__/helloworld.test.js
+++ __test__/helloworld.test.js
@@ -1,23 +1,19 @@
 const request = require('supertest');
 const operacionesService = require('../operation-service');
 describe('GET /', () => {
+
     it('responde con el resultado de a + b es = 5', done => {
         request(operacionesService)
             .get('/suma')
             .expect('la suma de a + b es = 5', done);
         done()
     });
-
-});
-describe('GET /', () => {
     it('responde con el resultado de la resta de a - b es = 1', done => {
         request(operacionesService)
             .get('/resta')
             .expect('la resta de a - b es = 1', done);
         done()
     });
-});
-describe('GET /', () => {
     it('responde con el resultado de la division de a / b es = 5', done => {
         request(operacionesService)
             .get('/division')
